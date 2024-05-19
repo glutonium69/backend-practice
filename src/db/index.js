3034ae535031e13a3ddd24d46158affccc2717f5
tahzib404 <tahzib404@gmail.com>
2024-05-19 12:19:33 +0000

    setting up routes and register controller

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,9 @@ import { DB_NAME } from "../constants.js";
 export const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("mongoDB connection successfull")
     } catch (error) {
-        console.log("MONGODB connection error", error);
+        console.log("MONGODB connection failed", error);
         process.exit(1);
     }
 }
