7dbf3e0f8575010cb1adab129379c396daf359f6
tahzib404 <tahzib404@gmail.com>
2024-05-24 03:20:56 +0000

    Added createPlaylist endpoint

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -4,5 +4,25 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { Playlist } from "../models/playlist.model.js";
 
 export const createPlaylist = asyncHandler(async (req, res) => {
+    const { name, description } = req.body;
 
+    if(!name || !description){
+        throw new ApiError(400, "Missing required fields: name and description");
+    }
+
+    const createdPlaylist = await Playlist.create({
+        name,
+        description,
+        owner: req?.user?._id
+    });
+
+    const playlist = await Playlist.findById(createdPlaylist?._id);
+
+    if(!playlist){
+        throw new ApiError(500, "Failed to create playlist");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, playlist, "Playlist creation successfull"))
 })
