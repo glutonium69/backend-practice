9d3e1df414a97eab9b0aabce98292ac940ee67af
tahzib404 <tahzib404@gmail.com>
2024-05-24 15:34:23 +0000

    Setting up controller and router file for comments

---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,21 @@
+import mongoose, { isValidObjectId } from "mongoose"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+export const getVideoComments = asyncHandler(async (req, res) => {
+
+})
+
+export const addComment = asyncHandler(async (req, res) => {
+
+})
+
+export const updateComment = asyncHandler(async (req, res) => {
+
+})
+
+export const deleteComment = asyncHandler(async (req, res) => {
+
+})
