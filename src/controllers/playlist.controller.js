9affd6b4ecd0155b21426b4da867c74d55b94dfa
tahzib404 <tahzib404@gmail.com>
2024-05-24 04:04:13 +0000

    Adding getUserPlaylists endpoint

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Playlist } from "../models/playlist.model.js";
+import mongoose, { isValidObjectId } from "mongoose";
 
 export const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
@@ -25,4 +26,58 @@ export const createPlaylist = asyncHandler(async (req, res) => {
     return res
     .status(200)
     .json(new ApiResponse(200, playlist, "Playlist creation successfull"))
+})
+
+export const getUserPlaylists = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "Invalid user ID");
+    }
+
+    const playlists = await Playlist.aggregate([
+        {
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "playlistVideos",
+                foreignField: "_id",
+                as: "playlistVideos",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        username: 1,
+                                        fullname: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, playlists[0].playlistVideos, "User playlistts fetched successfully"));
 })
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { createPlaylist, getUserPlaylists } from "../controllers/playlist.controller.js";
 
 export const playlistRouter = Router();
 
 playlistRouter.route("/createPlaylist").post(verifyJWT, createPlaylist);
+playlistRouter.route("/:userId").get(verifyJWT, getUserPlaylists);
