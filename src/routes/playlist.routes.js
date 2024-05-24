2c2e3d300e0c6733347ded5bac76d816f9bdfe53
tahzib404 <tahzib404@gmail.com>
2024-05-24 05:05:24 +0000

    typo fix: urser -> user

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -4,6 +4,6 @@ import { createPlaylist, getPlaylistById, getUserPlaylists } from "../controller
 
 export const playlistRouter = Router();
 
-playlistRouter.route("/urser/:userId").get(verifyJWT, getUserPlaylists);
+playlistRouter.route("/user/:userId").get(verifyJWT, getUserPlaylists);
 playlistRouter.route("/create").post(verifyJWT, createPlaylist);
 playlistRouter.route("/:playlistId").get(verifyJWT, getPlaylistById);
