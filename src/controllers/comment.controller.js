be94eb3e13004e187b3584bba7942329725166d1
tahzib404 <tahzib404@gmail.com>
2024-05-25 14:50:02 +0000

    Setting up addComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,6 @@
 import mongoose, { isValidObjectId } from "mongoose"
 import { Comment } from "../models/comment.model.js"
+import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
@@ -66,7 +67,35 @@ export const getVideoComments = asyncHandler(async (req, res) => {
 })
 
 export const addComment = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Invalid video id");
+    }
+
+    const { content } = req.body;
+
+    if (!content) {
+        throw new ApiError(400, "Content is required");
+    }
+
+    const comment = await Comment.create({
+        content,
+        owner: req.user?._id,
+        video: videoId
+    })
+
+    if (!comment) {
+        throw new ApiError(500, "Comment creation failure");
+    }
+
+    return res.status(200).json(new ApiResponse(200, comment, "Comment created"))
 })
 
 export const updateComment = asyncHandler(async (req, res) => {
