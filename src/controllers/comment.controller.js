93d397279f339e2d25cab3ed0c2be7611edb1990
tahzib404 <tahzib404@gmail.com>
2024-05-25 10:37:13 +0000

    Setting up getVideoComments controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,7 +5,64 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 export const getVideoComments = asyncHandler(async (req, res) => {
+    const { videoId } = req.params;
+    const { page = 1, limit = 2 } = req.query;
 
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const pipeLine = [
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            fullname: 1,
+                            avatar: {
+                                url: 1
+                            },
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$owner"
+                }
+            }
+        },
+        {
+            $sort: {
+                createdAt: 1
+            }
+        }
+    ]
+
+    const commentsPaginated = await Comment.aggregatePaginate(Comment.aggregate(pipeLine), {
+        page: Number(page),
+        limit: Number(limit)
+    })
+
+    if (!commentsPaginated) {
+        throw new ApiError("500", "Comments fetch failure");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, commentsPaginated, "Comments fetched"))
 })
 
 export const addComment = asyncHandler(async (req, res) => {
