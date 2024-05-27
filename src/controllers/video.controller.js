50382f792150efed4527876979f89fb02260a3f1
tahzib404 <tahzib404@gmail.com>
2024-05-27 11:38:07 +0000

    Adding updateVideo controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -9,10 +9,45 @@ import { uploadFileOnCloudinary, deleteFromCloudinary } from "../utils/cloudinar
 
 
 export const getAllVideos = asyncHandler(async (req, res) => {
-    const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
+    const {
+        page = 1,
+        limit = 10,
+        query = null,
+        sortBy = "createdAt",
+        sortType = "asc",
+        userId
+    } = req.query;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user id");
+    }
+
+    if (isNaN(parseInt(page)) || isNaN(parseInt(limit))) {
+        throw new ApiError(400, "Invalid page or limit. Please provide valid number");
+    }
+
+    const sortByOptions = [
+        "createdAt",
+        "title",
+        "views",
+        "length"
+    ];
+
+    if (!sortByOptions.includes(sortBy)) {
+        throw new ApiError(400, "Invalid sort by option. Available options: createdAt, title, views, length");
+    }
+
+    const sortTypeOptions = [
+        "asc",
+        "desc"
+    ];
+
+    if (!sortTypeOptions.includes(sortType)) {
+        throw new ApiError(400, "Invalid sort type option. Available options: asc, desc");
+    }
 
     return res
-    .status(200)
+        .status(200)
 })
 
 export const publishAVideo = asyncHandler(async (req, res) => {
@@ -117,20 +152,91 @@ export const getVideoById = asyncHandler(async (req, res) => {
 })
 
 export const updateVideo = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
+    const { videoId } = req.params;
+
+    if (!isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    if (video.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(400, "Invalid request. Request user must be owner of the video");
+    }
+
+    const {
+        title = undefined,
+        description = undefined,
+        visibility = video.isPublic ? "public" : "private"
+    } = req.body;
+
+    const thumbnailPath = req.file?.path;
+    
+    if (!title && !description && visibility === "public" && !thumbnailPath) {
+        throw new ApiError(400, "No changes made");
+    }
+
+    const thumbnailFileType = req.file?.mimetype;
+
+    if(thumbnailFileType && !thumbnailFileType.startsWith("image")){
+        throw new ApiError(400, "Thumbnail file must be an image file");
+    }
+
+    if (!["public", "private"].includes(visibility)) {
+        throw new ApiError(400, "Invalid visibility option. Available options: public, private");
+    }
+
+    let uploadedThumbnail = undefined;
+
+    if(thumbnailPath){
+
+        uploadedThumbnail = await uploadFileOnCloudinary(thumbnailPath);
+
+        if (!uploadedThumbnail) {
+            throw new ApiError(500, "Thumbnail upload failed");
+        }
+    }
+
+    const updatedVideo = await Video.findByIdAndUpdate(videoId,
+        {
+            $set: {
+                title: (title && title.trim() !== "") ? title.trim() : video.title,
+                description: description ? description.trim() : video.description,
+                isPublic: visibility === "public",
+                thumbnail: uploadedThumbnail ? uploadedThumbnail.url : video.thumbnail
+            }
+        },
+        { new: true }
+    );
+
+    if(updatedVideo && uploadedThumbnail){
+        // delete previous thumbnail from cloudinary if new thumbnail uploaded and video updated
+        const publicId = video.thumbnail.split("/").pop().split(".")[0];
+        await deleteFromCloudinary(publicId, "image");
+    }
+    else if(!updatedVideo && uploadedThumbnail){
+        // delete new thumbnail from cloudinary if new thumbanail uploaded but video didnt update
+        await deleteFromCloudinary(uploadedThumbnail.public_id, "image");
+        throw new ApiError(500, "Video update failed");
+    }
 
     return res
-    .status(200)
+        .status(200)
+        .json(new ApiResponse(200, updatedVideo, "Video updated successfully"))
 })
 
 export const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     return res
-    .status(200)
+        .status(200)
 })
 
 export const togglePublishStatus = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     return res
-    .status(200)
+        .status(200)
 })
