8c136910680cb7a372a455a9ca4055a347e5128e
tahzib404 <tahzib404@gmail.com>
2024-05-29 15:52:51 +0000

    Setting up dashboard route and controller

--- src/app.js
+++ src/app.js
@@ -21,9 +21,11 @@ import { userRouter } from "./routes/user.routes.js";
 import { playlistRouter } from "./routes/playlist.routes.js";
 import { commentRouter } from "./routes/comment.routes.js"
 import { videoRouter } from "./routes/video.routes.js";
+import { dashboardRouter } from "./routes/dashboard.routes.js";
 
 // /ai/v1/ will be our root url
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+export const getChannelVideos = asyncHandler(async (req, res) => {
+
+})
+
+export const getChannelStats = asyncHandler(async (req, res) => {
+
+})
